7fcc94311e2e2c6a1e82f51824299c0fda4e09ba
piaar <piaar.selling@gmail.com>
2025-02-11 19:02:30 +0900

    add log
    Former-commit-id: 5f4971a3f3d575c54b6d8dae7c997c989143dd39

--- proxy_config.py
+++ proxy_config.py
@@ -1,5 +1,10 @@
+import requests
+
 proxy = "http://54.180.117.185:13128"
 proxies = {
     "http": proxy,
     "https": proxy,
 }
+
+url = "https://piaarlab.store/"  # 여기에 요청을 보내고 싶다면
+resp = requests.get(url, proxies=proxies)
