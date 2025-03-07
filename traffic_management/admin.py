88a0e830638de495170da08efa46116ec810f45c
piaar <piaar.selling@gmail.com>
2025-03-07 11:05:05 +0900

    update rankings

---
+++ traffic_management/admin.py
@@ -0,0 +1,33 @@
+from django.contrib import admin
+from .models import Task, Product, Keyword, Traffic
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 
+        'product_name', 
+        'category', 
+        'keyword', 
+        'url', 
+        'current_rank', 
+        'difference_rank', 
+        'is_completed', 
+        'traffic', 
+        'created_at'
+    )
+    list_filter = (
+        'category', 
+        'is_completed', 
+        'traffic', 
+        'is_extended', 
+        'available_start_date', 
+        'available_end_date'
+    )
+    search_fields = (
+        'product_name', 
+        'category', 
+        'keyword__name', 
+        'url', 
+        'memo'
+    )
+    ordering = ('-created_at',)
